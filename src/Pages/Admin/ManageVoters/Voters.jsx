69f4cdc18af22d6ac13f112267c1aaa5a39e00c8
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2024-09-05 18:50:38 +0600

    New plan ready

--- src/Pages/Admin/ManageVoters/Voters.jsx
+++ src/Pages/Admin/ManageVoters/Voters.jsx
@@ -1,30 +1,48 @@
+import { MdDeleteOutline, MdOutlineSettings } from "react-icons/md";
 import useDepartment from "../../../Hooks/useDepartment";
 
 const Voters = () => {
   const [Department, isDepartmentLoading, refetch] = useDepartment();
-  console.log(Department);
+
   return (
-    <div className="join join-vertical w-full">
-      {Department?.map((department, i) => (
-        <div
-          key={i}
-          className="collapse collapse-arrow join-item border-base-300 border"
-        >
-          <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            Department Of {department.department}
-          </div>
-          <div className="collapse-content">
-            <div className="flex gap-3 flex-wrap justify-evenly">
-            {
-                department?.batch.map((b,j)=>(
-                    <button className=" px-4 py-2 bg-[#002a3f] text-white rounded" key={j}>{b}</button>
-                ))
-            }
-            </div>
-          </div>
-        </div>
-      ))}
+    <div>
+    <div className="flex justify-between flex-wrap my-5">
+        <div><h3 className="text-2xl font-semibold">List Of voters</h3></div>
+        <button className="btn btn-primary">Add new Voters</button>
+    </div>
+              <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Department</th>
+              <th>Program</th>
+              <th className="text-center">Batch</th>
+              <th className="text-center">Button</th>
+            </tr>
+          </thead>
+          <tbody>            
+            
+          {
+  Department?.map((e, index) => (
+    <tr className="hover" key={index}>
+      <th>{index + 1}</th>
+      <td>{e?.department}</td>
+      <td>{e?.program}</td>
+      <td className="text-center">{e?.batch.map(e=> `${e} `)}</td>
+      <td className="text-center">
+      <button className="text-white mx-1 bg-[#002a3f] w-auto py-1 px-4 text-2xl rounded hover:bg-[#2ec4b6] hover:text-[#002a3f] duration-300"><MdOutlineSettings /></button>
+
+      <button className="text-white mx-1 bg-red-600 w-auto py-1 px-4 text-2xl rounded hover:bg-red-700 duration-300"><MdDeleteOutline /></button>
+      </td>
+    </tr>
+  ))
+}
+          </tbody>
+        </table>
+
+      </div>
     </div>
   );
 };
